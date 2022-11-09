65e5d3b0e2e6beb50221e4cfab191a37fa9f4f79
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 17:12:40 +0100

    request was done

--- Combined Combine calls/Services/Requests.swift
+++ Combined Combine calls/Services/Requests.swift
@@ -5,12 +5,14 @@ class Requests {
     
     func loadUser() -> AnyPublisher<User, Error> {
         let url = URL(string: "http://a-url.com")!
-        URLSession.shared.dataTaskPublisher(for: url).tryMap { result in
+        return URLSession.shared.dataTaskPublisher(for: url).tryMap { result in
             guard let httpResponse = result.response as? HTTPURLResponse, httpResponse.statusCode <= 200 && httpResponse.statusCode < 300 else {
                 throw URLError(.badServerResponse)
             }
             return result.data
-        }.decode(type: User.self, decoder: JSONDecoder())
+        }
+        .decode(type: User.self, decoder: JSONDecoder())
+        .eraseToAnyPublisher()
     }
     
 }
