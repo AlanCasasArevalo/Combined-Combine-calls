d01825874afe38dca4552edf03bfad34b3834719
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 17:33:57 +0100

    Code was simplified

--- Combined Combine calls/Services/Requests.swift
+++ Combined Combine calls/Services/Requests.swift
@@ -15,38 +15,25 @@ class Requests {
     }
     
     func loadUser() -> AnyPublisher<User, Error> {
-        let url = URL(string: "http://a-user.com")!
-        return URLSession.shared.dataTaskPublisher(for: url).tryMap { result in
-            guard let httpResponse = result.response as? HTTPURLResponse, httpResponse.statusCode <= 200 && httpResponse.statusCode < 300 else {
-                throw URLError(.badServerResponse)
-            }
-            return result.data
-        }
-        .decode(type: User.self, decoder: JSONDecoder())
-        .eraseToAnyPublisher()
+        return load(url: URL(string: "http://a-user.com")!)
     }
     
     func loadDetails(user: User) -> AnyPublisher<UserDetails, Error> {
-        let url = URL(string: "http://a-user.com/\(user.id)/details")!
-        return URLSession.shared.dataTaskPublisher(for: url).tryMap { result in
-            guard let httpResponse = result.response as? HTTPURLResponse, httpResponse.statusCode <= 200 && httpResponse.statusCode < 300 else {
-                throw URLError(.badServerResponse)
-            }
-            return result.data
-        }
-        .decode(type: UserDetails.self, decoder: JSONDecoder())
-        .eraseToAnyPublisher()
+        return load(url: URL(string: "http://a-user.com/\(user.id)/details")!)
     }
     
     func loadFriends(user: User) -> AnyPublisher<[Friend], Error> {
-        let url = URL(string: "http://a-user.com/\(user.id)/friends")!
+        return load(url: URL(string: "http://a-user.com/\(user.id)/friends")!)
+    }
+    
+    func load<T: Decodable>(url: URL) -> AnyPublisher<T, Error> {
         return URLSession.shared.dataTaskPublisher(for: url).tryMap { result in
             guard let httpResponse = result.response as? HTTPURLResponse, httpResponse.statusCode <= 200 && httpResponse.statusCode < 300 else {
                 throw URLError(.badServerResponse)
             }
             return result.data
         }
-        .decode(type: [Friend].self, decoder: JSONDecoder())
+        .decode(type: T.self, decoder: JSONDecoder())
         .eraseToAnyPublisher()
     }
     
